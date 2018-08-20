16b989d2bc3e9edb173f2b9d8da1c65cb2111f5d
zcochran <fuzzylimes@gmail.com>
2018-08-19 20:21:09 -0400

    Updating folder structure

---
+++ tutorialapi-db/src/main/java/io/springboot/TutorialapiDbApplication.java
@@ -0,0 +1,12 @@
+package io.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialapiDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialapiDbApplication.class, args);
+	}
+}
---
+++ tutorialapi-db/src/main/java/io/springboot/topic/TopicController.java
@@ -0,0 +1,38 @@
+package io.springboot.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable("id") String id) {
+        return topicService.getTopic(id);
+    }
+
+    @PostMapping(value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @PutMapping(value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable("id") String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @DeleteMapping(value = "/topics/{id}")
+    public void deleteTopic(@PathVariable("id") String id) {
+        topicService.deleteTopic(id);
+    }
+}
